3aabb773b8fb057ef9ec8133dc568171212880c8
nangkhanrana <nangkhan.rana@gmail.com>
2025-12-17 10:00:00 +0545

    Implementing Admin API for viewing all orders and updating status

---
+++ controller/admin/user/review/reviewController.js
@@ -0,0 +1,92 @@
+const Review = require("../../../../model/reviewModel");
+const Product = require("../../../../model/productModel");
+
+exports.createReview = async (req,res)=>{
+    const userId= req.user.id;
+    const{rating, message} = req.body;
+    const productId = req.params.id;
+    if(!rating || !message || !productId){
+        return res.status(400).json({
+            message:"please provide everything"
+        })
+    }
+
+    const productExist = await Product.findById(productId);
+    if(!productExist){
+        return res.status(400).json({
+            message:"product with that productId doesnot exist"
+        })
+    }
+
+    await Review.create({
+        userId,
+        productId,
+        rating,
+        message,
+    });
+    res.status(200).json({
+        message:"Review added successfully"
+    })
+};
+
+exports.getProductReview = async (req, res) => {
+  const productId = req.params.id;
+  if (!productId) {
+    return res.status(400).json({
+      message: "Please provide productID",
+    });
+  }
+  const productExist = await Product.findById(productId);
+  if (!productExist) {
+    return res.status(404).json({
+      message: "product with that productId doesnt Exist",
+    });
+  }
+
+  const reviews = await Review.find({ productId })
+  .populate("userId")
+  .populate("productId");
+  res.status(200).json({
+    message: "review fetched successfully",
+    data: reviews,
+  });
+};
+
+exports.getMyReviews = async (req, res) => {
+  const userId = req.user.id;
+  const reviews = await Review.find({ userId });
+  if (reviews.length == 0) {
+    res.status(404).json({
+      message: "You havenot given review to any products yet",
+      reviews: [],
+    });
+  } else {
+    res.status(200).json({
+      message: "review fetched successfully",
+      data: reviews,
+    });
+  }
+};
+
+exports.deleteReview = async (req, res) => {
+  const reviewId = req.params.id;
+  //check if that User created this review
+  const userId = req.user.id;
+  if(!reviewId){
+    res.status(400).json({
+      message:"please provide reviewId"
+    });
+  }
+  const review = await Review.findById(reviewId);
+  const ownerIdofReview = review.userId;
+  if ((ownerIdofReview !== review.userId)) {
+    return res.status(400).json({
+      message: "You dont have permission to delete this review",
+    });
+  }
+
+  await Review.findByIdAndDelete(reviewId);
+  res.status(200).json({
+    message: "Review deleted successfully",
+  });
+};
